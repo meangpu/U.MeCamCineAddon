5ed824872cb1f495124b7496a6dd408999cbe72b
meangpu <51825872+meangpu@users.noreply.github.com>
2023-07-31 21:41:08 +0700

    perfect dolly

--- Runtime/DollyCamTrackRandom.cs
+++ Runtime/DollyCamTrackRandom.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace Meangpu
@@ -11,29 +12,51 @@ namespace Meangpu
         [SerializeField] Vector2 _waitTimeRandom = new(3, 7);
         [SerializeField] Vector2 _positionRandom = new(0, 0.04f);
         [SerializeField] Vector2 _speedRandom = new(0.08f, 0.15f);
+        [SerializeField] bool _isUseWaitRealTime;
 
-        public Cinemachine.CinemachineSmoothPath startPath;
-        public TrackWithTargetLookAt[] alterPath;
+        public List<TrackWithTargetLookAt> PathOption = new();
+        List<TrackWithTargetLookAt> _nowAvailablePath = new();
 
         void Start() => ResetPath();
 
         void ResetPath()
         {
             StopAllCoroutines();
-            _cart.m_Path = startPath;
+            SetupPathOption();
+            SetupCamAndPathByIndex(0);
             StartCoroutine(ChangeTrack());
         }
 
         IEnumerator ChangeTrack()
         {
-            yield return new WaitForSeconds(Random.Range(_waitTimeRandom.x, _waitTimeRandom.y));
+            if (_isUseWaitRealTime) yield return new WaitForSecondsRealtime(Random.Range(_waitTimeRandom.x, _waitTimeRandom.y));
+            else yield return new WaitForSeconds(Random.Range(_waitTimeRandom.x, _waitTimeRandom.y));
+
             _cart.m_Position = Random.Range(_positionRandom.x, _positionRandom.y);
             _cart.m_Speed = Random.Range(_speedRandom.x, _speedRandom.y);
-            TrackWithTargetLookAt nowTargetPath = alterPath[Random.Range(0, alterPath.Length)];
 
-            _cart.m_Path = nowTargetPath.path;
-            _virCam.LookAt = nowTargetPath.targetPrefab;
+            if (_nowAvailablePath.Count == 0) SetupPathOption();
+            int nowIndex = Random.Range(0, _nowAvailablePath.Count);
+
+            TrackWithTargetLookAt nowPath = SetupCamAndPathByIndex(nowIndex);
+
+            if (_nowAvailablePath.Contains(nowPath)) _nowAvailablePath.Remove(nowPath);
+
             StartCoroutine(ChangeTrack());
+        }
+
+        private TrackWithTargetLookAt SetupCamAndPathByIndex(int nowIndex)
+        {
+            TrackWithTargetLookAt nowPath = _nowAvailablePath[nowIndex];
+            _cart.m_Path = nowPath.path;
+            if (nowPath.targetPrefab != null) _virCam.LookAt = nowPath.targetPrefab;
+            return nowPath;
+        }
+
+        private void SetupPathOption()
+        {
+            _nowAvailablePath.Clear();
+            _nowAvailablePath = new(PathOption);
         }
 
         [System.Serializable]
